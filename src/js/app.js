254b6c202e5f9e3de7437f80beaeff375a0afbf1
warnimal <warnimal128@gmail.com>
2021-11-16 01:55:25 +0200

    das

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import "../scss/app.scss";
 
+var x = document.querySelectorAll(".hot");
+
+for (let index = 0; index < x.length; index++) {
+
+  x[index].innerHTML += "🔥";
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
