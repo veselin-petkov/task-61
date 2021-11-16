3776bebb99b66b826327b4a423f819a9ce50ddb3
warnimal <warnimal128@gmail.com>
2021-11-16 02:07:33 +0200

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ var x = document.querySelectorAll(".hot");
 
 for (let index = 0; index < x.length; index++) {
 
-  x[index].innerHTML += "🔥";
+  x[index].innerHTML += " 🔥";
 }
 
 window.addEventListener("DOMContentLoaded", () => {
